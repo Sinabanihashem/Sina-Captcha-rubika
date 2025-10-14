eb1b05b1e74b809d9b469759993922d46efd43b6
mir sina banihashem <four.x.cpn@gmail.com>
2025-10-14 18:56:54 +0330

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ async def main(message):
         return
 
     if text.lower() == "کپچا":
-        res = requests.get("https://sina-captcha.onrender.com/captcha")
+        res = requests.get("https://sina-api-captcha.onrender.com/captcha")
         data = res.json()
         image_data = base64.b64decode(data["captcha_base64"].split(",")[1])
         with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as tmp:
@@ -32,7 +32,7 @@ async def main(message):
             return
         code = text.split()[1]
         captcha_id = captcha_data[user_id]
-        res = requests.get(f"https://sina-captcha.onrender.com/captcha/verify?captcha_id={captcha_id}&user_input={code}")
+        res = requests.get(f"https://sina-api-captcha.onrender.com/captcha/verify?captcha_id={captcha_id}&user_input={code}")
         data = res.json()
         await message.reply(data["message"])
         del captcha_data[user_id]
